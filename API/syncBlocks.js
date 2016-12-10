9907f5851697e6c1d608e9113e67fc1989a95e68
Ivan Ivanov <kzv@narod.ru>
2016-12-10 14:04:48 +0000

    --

--- API/syncBlocks.js
+++ API/syncBlocks.js
@@ -39,11 +39,12 @@ exports.Sync = function()
                 console.log('start from '+heightStart);
                 g_utils.ForEachSync(aBlockNumbers, SaveBlock, function(){
                     //when all synced (or have error) then try again after 10 sec
+                    throw 'Block sync error 1';
                     setTimeout(exports.Sync, 10000);
                 }, function(err, nIndex, cbError){
                     //when one function return
                     if (err) {
-                        throw 'Block sync error';
+                        throw 'Block sync error 2';
                         cbError(true);
                         return;
                     }
