f355808237746797d6dceb7d3b0caddae1e5c865
root <root@ubuntu-512mb-nyc3-01.localdomain>
2017-05-03 19:10:41 +0000

    fix

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 'use strict';
 
-exports.my_port = process.env.PORT; //8088; //http port
+exports.my_port = 9090; //process.env.PORT; //8088; //http port
 exports.my_portSSL = 9443; //https port
 
 exports.dbName = './SmailCoinDB/blockchain.db';
