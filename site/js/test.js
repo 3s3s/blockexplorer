bbd1683ed836308659f074210de86a0a2e133c41
3s3s <ivanivanovkzv@gmail.com>
2017-06-28 12:02:56 +0000

    fix

--- site/js/test.js
+++ site/js/test.js
@@ -15,7 +15,7 @@ $('#form_getbalance').submit(function(e) {
     e.preventDefault();
     
     $.getJSON('/api/v1/address/balance/'+$('#address_getbalance').val(), (data) => {
-        if (data.status != 'success')
+        if (data.status != true)
         {
             $('#address_balance').html(data.message);
             return;
